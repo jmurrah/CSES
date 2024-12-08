a66cccd8c180ae741149bfdd5440c76a783257d7
jmurrah <jacobhmurrah@gmail.com>
2024-12-07 21:04:57 -0600

    update

--- weird_algorithm.py
+++ weird_algorithm.py
@@ -1,10 +1,7 @@
 number = int(input())
 
 while number != 1:
-    print(int(number))
-    if number % 2 == 0:
-        number /= 2
-    else:
-        number = (number * 3) + 1
+    print(number)
+    number = int(number / 2) if number % 2 == 0 else int((number * 3) + 1)
 
 print(1)
